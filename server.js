52e3abea546dff0d49d8f79a46efcc8308d0ba6a
Jayner1 <106836096+Jayner1@users.noreply.github.com>
2025-01-15 15:44:10 -0800

    home page working

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const static = require("./routes/static")
  * View Engine and Templates
  *************************/
 
-app.set("view.engine", "ejs")
+app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not as views root
 
@@ -24,6 +24,11 @@ app.set("layout", "./layouts/layout") // not as views root
  * Routes
  *************************/
 app.use(static)
+
+// Index Route
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
+})
 
 /* ***********************
  * Local Server Information
